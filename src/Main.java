0a8a2f1dcf46acb745f97fc5876b6d4c15a6c996
PedroAlbuquerque77 <145682595+PedroAlbuquerque77@users.noreply.github.com>
2023-09-21 15:51:37 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GIVE_NUMBER    = "GN";
+    public static final String EQUAL_PHONES    = "EP";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -24,6 +26,7 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -52,6 +55,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                 case GIVE_NUMBER:
+                    getNumber(in, cBook);
+                    break;
+                case EQUAL_PHONES:
+                    hasSameNumber(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +155,15 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void getNumber(Scanner in, ContactBook cBook) {
+        int phone;
+
+        phone = in.nextInt(); in.nextLine();
+        if (!cBook.hasContact(name)) {
+            System.out.println(CONTACT_ADDED);
+        }
+        else System.out.println(CONTACT_EXISTS);
     }
 }
